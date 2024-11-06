53acbe242f7e408e46fbe2654b086cf717f3c0ec
jwxiaoming <904158347@qq.com>
2024-11-06 13:05:21 +0800

    实现基础查询

--- list/views.py
+++ list/views.py
@@ -6,8 +6,9 @@ from list.models import Student, Grade_Class
 def index(request):
     students = Student.objects.all()
     grades = Grade_Class.objects.all()
-    for student in students:
-        print(student.id, student.student_text, student.class_text)
-    print('————————分割线————————')
-
+    # for student in students:
+    #     print(student.id, student.student_text, student.class_text)
+    # print('————————分割线————————')
+    for i in grades:
+        print(students.filter(class_text=i))
     return render(request, 'index.html', {'students': students, 'grades': grades})
